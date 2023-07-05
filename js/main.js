76063c6262cb845a61c6cde5373e2d6ac6bb5dbb
LisethEspinoza26 <liscores2020@gmail.com>
2023-07-05 16:47:33 -0500

    cambios en el boton dark-ya funciona

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-const menuItems = document.querySelectorAll('.item-menu');
-console.log(menuItems)
+const menuItems = document.querySelectorAll(".item-menu");
+console.log(menuItems);
 
 menuItems.forEach(function (item) {
-    item.addEventListener('click', function (e) {
-        const currentItem = document.querySelector('.active');
-        currentItem.classList.remove('active');
-        e.target.classList.add('active');
-    });
+  item.addEventListener("click", function (e) {
+    const currentItem = document.querySelector(".active");
+    currentItem.classList.remove("active");
+    e.target.classList.add("active");
+  });
 });
 
-const body = document.querySelector ("body");
-const iconos = document.querySelectorAll (".alinear-iconos");
-const boton = document.querySelector ("#btn-dark");
+const body = document.querySelector("body");
+const iconos = document.querySelectorAll(".alinear-iconos");
+const boton = document.querySelector("#btn-dark");
 
 boton.addEventListener("click", function (e) {
-    boton.classList.toggle ("on");
-    body.classList.toggle ("dark");
-    iconos.forEach(function (icono) {
-        icono.classList.add ("color_icon");
-    });
+  document.body.classList.toggle("dark");
+  boton.classList.toggle("active");
+  iconos.forEach(function (icono) {
+    icono.classList.add("color_icon");
+  });
 });
+
